37354d6918b20d6c268adc6956697891906c153b
Will Flores <willflores1221@gmail.com>
2022-07-12 20:18:00 -0400

    learned props

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Product from "./components/Product/Product";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Hello World</h1>
+			<div className="Product-items">
+				<Product
+					name="Google Home"
+					description="Your AI assistant"
+					price={59.99}
+				/>
+				<Product
+					name="Amazon Echo"
+					description="Alexa at your service"
+					price={19.99}
+				/>
+				<Product
+					name="Apple HomeKit"
+					description="The best home system"
+					price={189.99}
+				/>
+			</div>
+			<h1>hello world</h1>
+		</div>
+	);
 }
 
 export default App;
